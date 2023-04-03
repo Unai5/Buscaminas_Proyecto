b5c3df3d89678eb68c8786d01dcb60271c4d32a9
LuciaBV <luciabraga2001@gmail.com>
2023-04-03 10:59:16 +0200

    Actualización

--- BibliotecaDeClases/Tabla.vb
+++ BibliotecaDeClases/Tabla.vb
@@ -1,5 +1,21 @@
 ﻿Public Class Tabla
-    Public Property CantidadMinas() As Integer
-    Public Property LongitudTabla As Integer
+    Public Property CantidadMinas As Integer
+    Public Property Lado As Integer
+    Public Sub New(lado As Integer, minas As Integer)
+        _Lado = lado
+        CantidadMinas = minas
+        Dim botonesHorizontales() As Object = {}
+        Dim botonesVerticales() As Object = {}
+        For i = 0 To lado - 1
+            Array.Resize(botonesHorizontales, botonesHorizontales.Length + 1)
+            botonesHorizontales(botonesHorizontales.Length - 1) = New Casilla
+            For z = 0 To lado - 1
+                Array.Resize(botonesVerticales, botonesVerticales.Length + 1)
+                botonesVerticales(botonesVerticales.Length - 1) = New Casilla
+            Next
+        Next
+    End Sub
+
+
 
 End Class
