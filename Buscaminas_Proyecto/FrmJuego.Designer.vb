351c0bae260bc19e5147571e0e0ba4a28bd43880
LuciaBV <luciabraga2001@gmail.com>
2023-04-05 08:44:11 +0200

    -

--- Buscaminas_Proyecto/FrmJuego.Designer.vb
+++ Buscaminas_Proyecto/FrmJuego.Designer.vb
@@ -22,50 +22,50 @@ Partial Class FrmJuego
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TLPjuego = New System.Windows.Forms.TableLayoutPanel()
+        Me.tlpJjuego = New System.Windows.Forms.TableLayoutPanel()
         Me.SuspendLayout()
         '
-        'TLPjuego
+        'tlpJjuego
         '
-        Me.TLPjuego.ColumnCount = 10
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
-        Me.TLPjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.Location = New System.Drawing.Point(78, 49)
-        Me.TLPjuego.Margin = New System.Windows.Forms.Padding(2)
-        Me.TLPjuego.Name = "TLPjuego"
-        Me.TLPjuego.RowCount = 10
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
-        Me.TLPjuego.Size = New System.Drawing.Size(302, 327)
-        Me.TLPjuego.TabIndex = 0
+        Me.tlpJjuego.ColumnCount = 10
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.tlpJjuego.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.Location = New System.Drawing.Point(78, 49)
+        Me.tlpJjuego.Margin = New System.Windows.Forms.Padding(2)
+        Me.tlpJjuego.Name = "tlpJjuego"
+        Me.tlpJjuego.RowCount = 10
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32.0!))
+        Me.tlpJjuego.Size = New System.Drawing.Size(302, 327)
+        Me.tlpJjuego.TabIndex = 0
         '
         'FrmJuego
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(491, 441)
-        Me.Controls.Add(Me.TLPjuego)
+        Me.Controls.Add(Me.tlpJjuego)
         Me.Name = "FrmJuego"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents TLPjuego As TableLayoutPanel
+    Friend WithEvents tlpJjuego As TableLayoutPanel
 End Class
--- Buscaminas_Proyecto/FrmJuego.vb
+++ Buscaminas_Proyecto/FrmJuego.vb
@@ -1,18 +1,10 @@
 ﻿Imports BibliotecaDeClases
 Public Class FrmJuego
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub FrmJuego_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
-
-    End Sub
-
-    Private Sub TableLayoutPanel1_Paint_1(sender As Object, e As PaintEventArgs)
-
-    End Sub
-
-    Private Sub TableLayoutPanel1_Paint_2(sender As Object, e As PaintEventArgs) Handles TLPjuego.Paint
+    Private Sub TLPjuego_Paint(sender As Object, e As PaintEventArgs) Handles tlpJjuego.Paint
 
     End Sub
 End Class
