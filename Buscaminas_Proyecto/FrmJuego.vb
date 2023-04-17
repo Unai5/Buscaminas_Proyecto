906d41621bcd3c1e0c105d5b9ef9688880835bff
Unai V.T <unaiv2004@gmail.com>
2023-04-17 08:46:05 +0200

    jhb

--- Buscaminas_Proyecto/FrmJuego.vb
+++ Buscaminas_Proyecto/FrmJuego.vb
@@ -1,7 +1,7 @@
 ﻿Imports BibliotecaDeClases
 Public Class FrmJuego
 
-    Private botones As Button()()
+    Private botones As Button(9)(9)
 
 
 
@@ -10,7 +10,18 @@ Public Class FrmJuego
 
         tabla = New TableLayoutPanel
 
+        For i = 0 To botones.length - 1
+            For j = 0 To botones.length - 1
+                Public btn & i & j As New Button 
+            Next
+        Next
+
+
+
     End Sub
+
+
+
 
 
     Private Sub tlpJjuego_Paint(sender As Object, e As PaintEventArgs)
