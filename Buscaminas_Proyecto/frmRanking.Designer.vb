8db57a2f6451039520652f6ca92b01ef63f58878
Inazio <inazio.astigarraga@gmail.com>
2023-04-19 08:42:50 +0200

    frmRanking

--- Buscaminas_Proyecto/frmRanking.Designer.vb
+++ Buscaminas_Proyecto/frmRanking.Designer.vb
@@ -23,35 +23,35 @@ Partial Class frmRanking
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lblRanking = New System.Windows.Forms.Label()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.lstRanking = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'lblRanking
         '
         Me.lblRanking.AutoSize = True
         Me.lblRanking.Font = New System.Drawing.Font("Cooper Black", 12.0!)
-        Me.lblRanking.Location = New System.Drawing.Point(316, 35)
+        Me.lblRanking.Location = New System.Drawing.Point(333, 42)
         Me.lblRanking.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblRanking.Name = "lblRanking"
         Me.lblRanking.Size = New System.Drawing.Size(118, 23)
         Me.lblRanking.TabIndex = 4
         Me.lblRanking.Text = "RANKING"
         '
-        'ListBox1
+        'lstRanking
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(208, 133)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(324, 196)
-        Me.ListBox1.TabIndex = 5
+        Me.lstRanking.FormattingEnabled = True
+        Me.lstRanking.ItemHeight = 16
+        Me.lstRanking.Location = New System.Drawing.Point(222, 100)
+        Me.lstRanking.Name = "lstRanking"
+        Me.lstRanking.Size = New System.Drawing.Size(346, 340)
+        Me.lstRanking.TabIndex = 5
         '
         'frmRanking
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(796, 586)
-        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.lstRanking)
         Me.Controls.Add(Me.lblRanking)
         Me.Name = "frmRanking"
         Me.Text = "frmRanking"
@@ -61,5 +61,5 @@ Partial Class frmRanking
     End Sub
 
     Friend WithEvents lblRanking As Label
-    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents lstRanking As ListBox
 End Class
