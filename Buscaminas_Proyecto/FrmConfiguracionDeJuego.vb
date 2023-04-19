d5ff2c0cb6a7adce48d9448045573d70db02463e
Inazio <inazio.astigarraga@gmail.com>
2023-04-19 09:27:03 +0200

    Frm config

--- Buscaminas_Proyecto/FrmConfiguracionDeJuego.vb
+++ Buscaminas_Proyecto/FrmConfiguracionDeJuego.vb
@@ -5,6 +5,7 @@
     Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
         If Not radButtFacil.Checked AndAlso Not radButtNormal.Checked AndAlso Not radButtDificil.Checked Then
             MessageBox.Show("Selecciona una dificultad de juego")
+            Exit Sub
         End If
         If radButtFacil.Checked Then
             dificultad = 1
@@ -13,6 +14,7 @@
         ElseIf radButtDificil.Checked Then
             dificultad = 3
         End If
+        FrmJuego.Show()
     End Sub
     Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
         Me.Close()
