0e6d279e1174c43167bad2f20649ef40ac3414cb
Inazio <inazio.astigarraga@gmail.com>
2023-04-20 10:20:48 +0200

    .

--- Buscaminas_Proyecto/My Project/Application.Designer.vb
+++ Buscaminas_Proyecto/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Buscaminas_Proyecto.FrmInicioDeSesion
+            Me.MainForm = Global.Buscaminas_Proyecto.FrmJuego
         End Sub
     End Class
 End Namespace
