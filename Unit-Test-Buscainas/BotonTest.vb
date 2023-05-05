613e3d9107bbb0a0dd380059fbee7f1e43bd6b79
LuciaBV <luciabraga2001@gmail.com>
2023-05-05 20:26:23 +0200

    .

--- Unit-Test-Buscainas/BotonTest.vb
+++ Unit-Test-Buscainas/BotonTest.vb
@@ -1,9 +1,9 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
-<TestClass()> Public Class UnitTest1
+<TestClass()> Public Class BotonTest
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub BotonConstructorTest()
     End Sub
 
 End Class
