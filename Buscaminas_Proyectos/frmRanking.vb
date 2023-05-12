cc44f647edd51bf9ee38aa177e06353ffa2278d3
LuciaBV <luciabraga2001@gmail.com>
2023-05-12 13:28:37 +0200

    .

--- Buscaminas_Proyectos/frmRanking.vb
+++ Buscaminas_Proyectos/frmRanking.vb
@@ -53,65 +53,16 @@
     End Sub
 
     Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
-        If btnDificultad.Visible = True Then
-            Me.Close()
-            FrmConfiguracionDeJuego.Show()
-        Else
-        End If
-        lstRanking.Items.Clear()
-        lblRanking.Text = "RANKING FÁCIL"
-        btnDificultad.Text = "Dificultad: Media"
-        If gestionUsrs.Usuarios Is Nothing Then
-            MessageBox.Show("No hay usuarios todavía")
-        Else
-            For Each per In gestionUsrs.Usuarios
-                lstRanking.Items.Add($"{per.Nombre} ...............{per.MejorTiempoFacil}")
-            Next
-        End If
-        btnDificultad.Visible = True
-        btnDificultad.Location = New Point(56, 384)
-        btnVolver.Location = New Point(346, 386)
-
-    End Sub
-    Private Sub lstRanking_DoubleClick(sender As Object, e As EventArgs) Handles lstRanking.DoubleClick
-        If lstRanking.SelectedItem = "" OrElse lstRanking.SelectedItem Is Nothing Then
-            MessageBox.Show($"No has seleccionado a nadie", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-        Else
-            btnDificultad.Visible = False
-            btnVolver.Location = New Point(217, 386)
-            lblRanking.Text = $"RANKING DE {lstRanking.SelectedItem.ToString.ToUpper}"
-            If lstRanking.SelectedItem.ToString.Length > 10 Then
-                lblRanking.Location = New Point(225 - (lblRanking.Width / 4), 22)
-            Else
-                lblRanking.Location = New Point(225 - (lblRanking.Width / 6), 22)
-            End If
-            If gestionUsrs.Usuarios Is Nothing Then
-                MessageBox.Show("No hay usuarios todavía")
-            Else
-                'Encontrar usuario
-                For Each tiempo In gestionUsrs.Usuarios
-                    'lstRanking.Items.Add($"{per.Nombre} ...............{per.MejorTiempoFacil}")
-                Next
-            End If
-        End If
+        FrmConfiguracionDeJuego.Show()
+        Me.Visible = False
     End Sub
 
-    Private Sub lstRanking_MouseHover(sender As Object, e As EventArgs) Handles lstRanking.MouseHover
-        Dim linea As Control = TryCast(sender, Control)
-        If linea Is Nothing Then
-            Exit Sub
-        Else
-            linea.BackColor = Color.Aquamarine
-            linea.Font = New Font("Rockwell", 11, FontStyle.Bold)
-        End If
-    End Sub
-    Private Sub lstRanking_MouseLeave(sender As Object, e As EventArgs) Handles lstRanking.MouseLeave
-        Dim linea As Control = TryCast(sender, Control)
-        If linea Is Nothing Then
-            Exit Sub
-        Else
-            linea.BackColor = Color.White
-            linea.Font = New Font("Rockwell", 11, FontStyle.Regular)
+    Private Sub FrmInicioSesion_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        Dim resp As DialogResult
+        resp = MessageBox.Show("¿Estas seguro de cerrar el juego?", "Atención", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If resp = Windows.Forms.DialogResult.No Then
+            e.Cancel = True
         End If
     End Sub
 End Class
