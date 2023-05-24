c107dc57dcaa435f4854e05e9fac80a2c03fb487
LuciaBV <luciabraga2001@gmail.com>
2023-05-24 12:35:02 +0200

    .

--- Buscaminas_Proyectos/My Project/Application.Designer.vb
+++ Buscaminas_Proyectos/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Buscaminas_Proyectos.FrmRanking
+            Me.MainForm = Global.Buscaminas_Proyectos.FrmInicioDeSesion
         End Sub
     End Class
 End Namespace
